825f85cc31faa39f505c2895c2c07ed4ee36b915
Warner-Felicia <foodleswarner@gmail.com>
2021-11-05 20:40:47 -0400

    added passwordReset form

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,3 +4,10 @@ exports.getLogin = (req, res, next) => {
         path: '/auth/login'
     });
 };
+
+exports.getReset = (req, res, next) => {
+    res.render('auth/passwordReset',{
+        pageTitle: 'Password Reset',
+        path: '/auth/reset'
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const authController = require('../controllers/auth');
 
 router.get('/login', authController.getLogin);
 
+router.get('/reset', authController.getReset);
+
 module.exports = router;
